20d847cdfd22a007a25e97ae5c4ea04a44449e18
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2017-02-09 09:40:18 +0100

    fix backup script

--- bin/backup.sh
+++ bin/backup.sh
@@ -10,4 +10,4 @@ mkdir -p ./backup
 docker-compose exec $1 sh -c \
 "mysqldump --all-databases --add-drop-table -u root -pcprt87 | gzip -c -9 > /backup/$1.db.bak.sql.gz"
 
-tar -c -O ./backup/dev | gzip -c -9 > ./backup/$1.wp.bak.tar.gz
+tar -c -O ./backup/$1 | gzip -c -9 > ./backup/$1.wp.bak.tar.gz
